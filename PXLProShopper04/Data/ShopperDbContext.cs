dbb0d6a7057e8ceb142d7285e14c13107c822b5a
ItzBob <51436202+SvenN94@users.noreply.github.com>
2023-03-07 11:12:13 +0100

    DB context created Added Models Made Connectionstring Added Use.Authentication

---
+++ PXLProShopper04/Data/ShopperDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PXLProShopper04.Models;
+
+namespace PXLProShopper04.Data
+{
+    public class ShopperDbContext : IdentityDbContext
+    {
+        public ShopperDbContext(DbContextOptions<ShopperDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Skins> Skins { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
---
+++ PXLProShopper04/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PXLProShopper04.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public int? LoginId { get; set; }
+        public int? SkinId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public int? Amount { get; set; }
+    }
+}
